80654564d9695954a435bc7c2750d887f154b16f
Sergey Safonov <spoof@spoofa.info>
2014-11-27 23:35:27 +0300

    Use unittests

--- functional_tests.py
+++ functional_tests.py
@@ -1,9 +1,31 @@
+import unittest
+
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:5000')
 
-elem = browser.find_element_by_xpath("//*")
-source_code = elem.get_attribute("outerHTML")
+class VisitorTest(unittest.TestCase):
 
-assert 'Hello World!' in source_code
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_fresh_start_page(self):
+        self.browser.get('http://localhost:5000')
+
+        self.assertIn('Currency Converter', self.browser.title)
+        self.fail('Finish the test!')
+        # User inputs data into 'Amount' field
+
+        # user select value from 'From currency' field
+
+        # user select value from 'To currency' field
+
+        # users clicks on 'Convert' button
+
+        # users sees in '#result' result
+
+if __name__ == '__main__':
+    unittest.main()
