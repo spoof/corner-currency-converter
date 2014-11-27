4a2486a0b4e871244c1dd6475b8e829403edf9b5
Sergey Safonov <spoof@spoofa.info>
2014-11-28 01:25:52 +0300

    Added styles

--- currency_converter/forms.py
+++ currency_converter/forms.py
@@ -6,4 +6,4 @@ class ConvertForm(Form):
     amount = FloatField('Amount', [validators.InputRequired(),
                                    validators.NumberRange(min=0, max=10000)])
     convert_from = SelectField('From', choices=[('rub', 'RUB'), ('usd', 'USD')])
-    convert_to = SelectField('From', choices=[('rub', 'RUB'), ('usd', 'USD')])
+    convert_to = SelectField('To', choices=[('rub', 'RUB'), ('usd', 'USD')])
